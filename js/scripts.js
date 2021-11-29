469ec325499c2be25903c5bf9b5f68f0c8d29a3f
nallanivn <anigam791@gmail.com>
2021-11-30 02:13:13 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -219,24 +219,24 @@ $(document).ready(function() {
     },
     data: {
       // Event title
-      title: "Ram and Antara's Wedding",
+      title: "Reeti and Anshul's Wedding",
 
       // Event start date
-      start: new Date("Nov 27, 2017 10:00"),
+      start: new Date("DEC 7, 2021 17:00"),
 
       // Event duration (IN MINUTES)
       // duration: 120,
 
       // You can also choose to set an end time
       // If an end time is set, this will take precedence over duration
-      end: new Date("Nov 29, 2017 00:00"),
+      end: new Date("DEC 7, 2021 23:00"),
 
       // Event Address
-      address: "ITC Fortune Park Hotel, Kolkata",
+      address: "Diamond galaxy party lawn, Kanpur",
 
       // Event Description
       description:
-        "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+        "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Dheeraj at +91 9792066660."
     }
   });
 
